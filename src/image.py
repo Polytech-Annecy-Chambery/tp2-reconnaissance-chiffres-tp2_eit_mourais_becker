d6df4040945cc571eeff695e47801c8d94c6af34
omimmou <95041251+omimmou@users.noreply.github.com>
2022-01-10 16:53:17 +0100

    Update image.py

--- src/image.py
+++ src/image.py
@@ -47,7 +47,22 @@ class Image:
     #   on retourne une nouvelle image binarisee
     #==============================================================================
     def binarisation(self, S):
-        pass
+        # preparaton du resultat : creation d'une image vide 
+        im_modif = image()
+        # affectation de l'image resultat par un tableau de 0, de meme taille
+        # que le tableau de pixels de l'image self
+        # les valeurs sont de type uint8 (8bits non signes)
+        im_modif.set_pixels(np.zeros((self.H,self.W), dtype=np.uint8))
+                                                
+        # boucle imbriquees pour parcourir tous les pixels de l'image
+        for l in range(self.H):
+            for c in range(self.W):
+                # modif des pixels d'intensite >= a S
+                if self.pixels[l][c] >= S:
+                    im_modif.pixels[l][c] = 255
+                else :
+                    im_modif.pixels[l][c] = 0
+        return im_modif
 
 
     #==============================================================================
@@ -59,13 +74,51 @@ class Image:
     #   on retourne une nouvelle image recadree
     #==============================================================================
     def localisation(self):
-        pass
+       # preparaton du resultat : creation d'une image vide 
+        im_modif = image()
+        
+        # initialisation des coordonnÃ©es maximum Ã  0 et minimm Ã  la valeur
+        # de la dimension correspondante
+        lmax = 0
+        lmin = self.H
+        cmax = 0
+        cmin = self.W
+                                                
+        # boucle imbriquees pour parcourir tous les pixels de l'image
+        for l in range(self.H):
+            for c in range(self.W):
+                # recherche des coordonnÃ©es maximum
+                if self.pixels[l][c] == 0:
+                    if l > lmax:
+                        lmax = l
+                    if c > cmax:
+                        cmax = c
+                # recherche des coordonÃ©es minimum
+                    if l < lmin:
+                        lmin = l
+                    if c < cmin:
+                        cmin = c
+        
+        # remplissage de l'image avec l'image self qui est elle meme recadrÃ©e
+        # avec les coordonnÃ©es trouvÃ©es
+        im_modif.set_pixels(self.pixels[lmin:lmax+1,cmin:cmax+1])
+        return im_modif
 
     #==============================================================================
     # Methode de redimensionnement d'image
     #==============================================================================
     def resize(self, new_H, new_W):
-        pass
+       # preparaton du resultat : creation d'une image vide 
+        im_modif = image()
+        
+        # crÃ©ation d'un tableau x reprenant les valeurs du tableau de l'image 
+        # initiale et de la taille voulue
+        x = resize(self.pixels, (new_H,new_W), 0)
+        x = np.uint8(x*255)
+        
+        # remplissage de l'image avec le tableau x
+        im_modif.set_pixels(x)
+        return im_modif
 
 
     #==============================================================================
